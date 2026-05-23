beeae605e1d5c90ed3e213247667a9a807746040
ENTRENA CONSULTING SAS <chmedinag@gmail.com>
2026-05-22 20:04:43 -0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/informe-de-inspeccion-gtc-45/' : '/',
+  base: './',
   resolve: {
     alias: {
       '@': '/src',
