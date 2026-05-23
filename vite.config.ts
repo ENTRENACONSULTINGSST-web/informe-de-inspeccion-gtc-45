8a9b097b244ffe5e81de820ec5c02e58699d068d
Entrena <entrenaconsulting@gmail.com>
2026-05-22 20:39:15 -0500

    Fix: Agregar postcss.config.cjs para resolver build de Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/informe-de-inspeccion-gtc-45/',
+  base: "/informe-de-inspeccion-gtc-45/",
   css: {
-    postcss: './postcss.config.cjs',
+    postcss: "./postcss.config.cjs",
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
 })
